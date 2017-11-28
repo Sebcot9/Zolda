36c6db831e63acde64edbb61c09a50d6ba79279a
Sébastien Cotor <seb.cotor@gmail.com>
2017-11-28 14:24:11 +0100

    Ajout d'éléments dans le viewer

--- src/ui/Viewer.java
+++ src/ui/Viewer.java
@@ -1,5 +1,36 @@
 package ui;
 
-public class Viewer {
+import specifications.ReadService;
+import specifications.RequireReadService;
+import specifications.ViewerService;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 
+public class Viewer implements ViewerService, RequireReadService{
+
+	private ReadService data;
+	private double xShrink, yShrink, xModifier, yModifier;
+	@Override
+	public void bindReadService(ReadService service)
+	{
+		data=service;
+	}
+	
+	@Override
+	public void init(){
+		
+	}
+	
+	@Override
+	public Parent getPanel(){
+		Group panel = new Group();
+		return panel;
+		
+	}
+	
+	@Override
+	public void setMainWindowWidth(double w){}
+	
+	@Override
+	public void setMainWindowHeight(double h){}
 }
